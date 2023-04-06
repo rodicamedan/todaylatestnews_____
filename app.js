27c9b3a9d54ccf0b4572f03b7890d4b29bf55ffa
rodicamedan <72512520+rodicamedan@users.noreply.github.com>
2023-04-06 19:54:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-import fetch from "node-fetch";
-import http from "http";
+import express from "express";
 import { readFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-const hostname = "127.0.0.1";
-const port = 80;
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const publicDir = join(__dirname, "public");
 
-http
-  .createServer(async (req, res) => {
-    if (req.url === "/") {
-      try {
-        const html = await readFile("./public/index.html");
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        res.end(html);
-      } catch (err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.end("Server Error");
-      }
-    } else if (req.url === "/script.js") {
-      try {
-        const js = await readFile("./public/script.js");
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/javascript");
-        res.end(js);
-      } catch (err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.end("Server Error");
-      }
-    } else {
-      res.statusCode = 404;
-      res.end("Page Not Found");
-    }
-  })
-  .listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+app.get("/", async (req, res) => {
+  try {
+    const html = await readFile(join(publicDir, "index.html"));
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(html);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end("Server Error");
+  }
+});
+
+app.get("/script.js", async (req, res) => {
+  try {
+    const js = await readFile(join(publicDir, "script.js"));
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/javascript");
+    res.end(js);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end("Server Error");
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
